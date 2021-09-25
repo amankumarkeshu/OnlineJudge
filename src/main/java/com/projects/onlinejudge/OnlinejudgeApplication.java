210283ece8c0e673f941eb01902de6905a4b310c
Akash Shrivastva <akash.shrivastva@gmail.com>
2021-09-25 20:32:39 +0530

    first commit

---
+++ src/main/java/com/projects/onlinejudge/OnlinejudgeApplication.java
@@ -0,0 +1,13 @@
+package com.projects.onlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinejudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinejudgeApplication.class, args);
+	}
+
+}
