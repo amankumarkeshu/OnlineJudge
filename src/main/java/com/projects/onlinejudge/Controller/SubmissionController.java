4c83b43bd794c028ab33b8d94d01f931be68bb47
Aman Kumar <amankumar.k@commerceiq.ai>
2021-10-02 22:00:58 +0530

    SubmissionController: Added Python and java Runners

--- src/main/java/com/projects/onlinejudge/Controller/SubmissionController.java
+++ src/main/java/com/projects/onlinejudge/Controller/SubmissionController.java
@@ -5,10 +5,14 @@ import com.projects.onlinejudge.dto.RunResponse;
 import com.projects.onlinejudge.helpers.IRunner;
 import com.projects.onlinejudge.helpers.LanguageFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/submit")
@@ -23,8 +27,11 @@ public class SubmissionController {
         System.out.println("User ID  :" +attribute.getUserId());
         System.out.println("Submission ID :" +attribute.getSubmissionId());
         System.out.println("User Language :" +attribute.getLang());
+        String lang= attribute.getLang();
+        ArrayList<String> languagesAvailable = new ArrayList<>(Arrays.asList("cpp", "java", "python"));
+        Assert.isTrue (languagesAvailable.contains(lang), "Wrong Language Provided. Please try from any of the following [cpp, java, python]\n"); // : "Wrong Language Provided. Please try from any of the following [cpp, java, python]";
 
-        IRunner runnerFactory = languageFactory.getLangRunner(attribute.getLang());
+        IRunner runnerFactory = languageFactory.getLangRunner(lang);
         RunResponse runResponse = runnerFactory.runTests(attribute);
         return runResponse;
     }
